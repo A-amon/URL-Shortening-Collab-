88f3453fa030f3b0cc9a58973986a91660f25580
A-amon <64846892+A-amon@users.noreply.github.com>
2021-09-07 05:36:23 +0800

    Add animation upon intersection

---
+++ index.js
@@ -0,0 +1,23 @@
+const boostSection = document.querySelector('.boost')
+
+/**
+ * Observe intersection for .boost section
+ * Animate on intersect
+ */
+const observeBoostIntersect = () => {
+	const observer = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+			// intersects Boost
+			if (entry.intersectionRatio > 0) {
+				boostSection.classList.add('anim')
+			}
+			else {
+				boostSection.classList.remove('anim')
+			}
+		})
+	})
+
+	observer.observe(boostSection)
+}
+
+observeBoostIntersect()
