7281c01f8c112f751dacc632e6ffc154123cbc17
BHD-Ilyes <ilyeshajdahman81@gmail.com>
2021-09-12 18:08:06 +0100

    finish navbar && hero section

--- index.js
+++ index.js
@@ -1,39 +1,45 @@
-const boostSection = document.querySelector('.boost')
+const boostSection = document.querySelector(".boost");
 
 /**
  * Observe intersection for .boost section
  * Animate on intersect
  */
 const observeBoostIntersect = () => {
-	const observer = new IntersectionObserver(entries => {
-		entries.forEach(entry => {
-			// intersects Boost
-			if (entry.intersectionRatio > 0) {
-				boostSection.classList.add('anim')
-			}
-			else {
-				boostSection.classList.remove('anim')
-			}
-		})
-	})
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      // intersects Boost
+      if (entry.intersectionRatio > 0) {
+        boostSection.classList.add("anim");
+      } else {
+        boostSection.classList.remove("anim");
+      }
+    });
+  });
 
-	observer.observe(boostSection)
-}
+  observer.observe(boostSection);
+};
 
-observeBoostIntersect()
+observeBoostIntersect();
 
 /**
  * Setup particles js animation
  */
-particlesJS.load('particles-js', 'particles.json')
+particlesJS.load("particles-js", "particles.json");
 
 //toggle navigation bar
-document.getElementById('toggle').addEventListener('click', function() {
-	let nav = document.getElementById("nav");
-	if(nav.classList == "test"){
-		nav.classList.add("nav");
-	}else{
-		nav.classList.remove("nav");
-	}
-	console.log('hello')
-})
+let toggle = document.getElementById("toggle");
+toggle.addEventListener("click", function () {
+  let nav = document.getElementById("nav");
+  if (nav.classList == "nav") {
+    nav.classList.add("add-nav");
+    let img = document.getElementById("img");
+    img.classList.add("rotate");
+    img.classList.remove("in-rotate");
+    img.setAttribute("src", "assets/icons/icon-close.svg");
+  } else {
+    nav.classList.remove("add-nav");
+    img.setAttribute("src", "assets/icons/icon-hamburger.svg");
+    img.classList.add("in-rotate");
+    img.classList.remove("rotate");
+  }
+});
